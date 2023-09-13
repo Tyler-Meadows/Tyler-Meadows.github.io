b439af1b7054bab4a8c8248f5547bb6c752bf252
Tyler Meadows <tam15@queensu.ca>
2023-09-13 19:34:12 -0400

    Lecture 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,3 +128,14 @@ navigationLinks.forEach(navLink => {
     navLink.click()
   }
 })
+
+// Show Solutions on Click
+function showSolution(id) {
+  var x = document.getElementById(id);
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
+}
